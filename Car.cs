53a4409b45ac4fada530e2e64e7c3ff874f114aa
D0mazo <dominykas.simanavicius@proton.me>
2024-04-18 15:19:38 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    public class Car
+    {
+        public Engine Engine { get; set; }
+        public string Model { get; set; }
+        public Cargo Cargo { get; set; }
+        public List<Tire> Tires { get; set; }
+
+        public Car
+            (string model,
+            int engineSpeed, int enginePower,
+            int cargoWeight, string cargoType,
+            double tire1Pressure, double tire2Pressure, double tire3Pressure, double tire4Pressure,
+            int tire1Age, int tire2Age, int tire3Age, int tire4Age)
+        {
+            Model = model;
+            Engine = new Engine(engineSpeed, enginePower);
+            Cargo = new Cargo(cargoWeight, cargoType);
+            Tires = new List<Tire>();
+            Tires.Add(new Tire(tire1Pressure, tire1Age));
+            Tires.Add(new Tire(tire2Pressure, tire2Age));
+            Tires.Add(new Tire(tire3Pressure, tire3Age));
+            Tires.Add(new Tire(tire4Pressure, tire4Age));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Temp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\Domin\OneDrive\Desktop\BlazorApp\Temp\Masina.txt";
+            string[] lines = File.ReadAllLines(file);
+            var vehicleCount = int.Parse(lines[0]);
+            var vehiclesList = new List<Car>();
+
+            for (int i = 1; i <= vehicleCount; i++)
+            {
+                var vehicleInformation = lines[i].Split(' ');
+
+                var model = vehicleInformation[0];
+                var engineSpeed = int.Parse(vehicleInformation[1]);
+                var enginePower = int.Parse(vehicleInformation[2]);
+                var cargoWeight = int.Parse(vehicleInformation[3]);
+                var cargoType = vehicleInformation[4];
+                var tire1Pressure = double.Parse(vehicleInformation[5]);
+                var tire1Age = int.Parse(vehicleInformation[6]);
+                var tire2Pressure = double.Parse(vehicleInformation[7]);
+                var tire2Age = int.Parse(vehicleInformation[8]);
+                var tire3Pressure = double.Parse(vehicleInformation[9]);
+                var tire3Age = int.Parse(vehicleInformation[10]);
+                var tire4Pressure = double.Parse(vehicleInformation[11]);
+                var tire4Age = int.Parse(vehicleInformation[12]);
+
+                var car = new Car(model, engineSpeed, enginePower,
+                    cargoWeight, cargoType,
+                    tire1Pressure, tire2Pressure, tire3Pressure, tire4Pressure,
+                    tire1Age, tire2Age, tire3Age, tire4Age);
+
+                vehiclesList.Add(car);
+            }
+
+            BadTires(vehiclesList);
+        }
+
+        static void BadTires(List<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                foreach (var tire in car.Tires)
+                {
+                    if (tire.Pressure < 1)
+                    {
+                        Console.WriteLine($"{car.Model}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
